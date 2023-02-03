fa9e6bbc3d34f636063c84e8f02ec6172e72a819
andre <andre.jacques.pujol@gmail.com>
2023-02-03 08:55:26 -0600

    Finished with game, working as intended

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 let score = 0;
+
+const gameArea = document.getElementById("whack-a-mole");
+const scoreP = document.getElementById("score");
+
+gameArea.addEventListener(`click`, function (e) {
+  if (e.target.matches(".mole")) {
+    score += 1;
+    scoreP.innerText = score;
+    e.target.classList.toggle(`mole`);
+  }
+});
+
+let holes = document.getElementsByClassName("hole");
+holes = [...holes];
+console.log(holes);
+
+let interval = setInterval(toggleMole, 500);
+
+function toggleMole() {
+  let randomNumber = Math.floor(Math.random() * holes.length);
+  console.log(`Random Number: ${randomNumber}`);
+  holes[randomNumber].classList.toggle(`mole`);
+}
